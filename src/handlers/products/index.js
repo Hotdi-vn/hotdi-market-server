0d78c550cde13cf93610daae07849e84e3b27027
Huy <xuanhuypham1501@gmail.com>
2024-01-30 00:32:02 +0700

    feat: add verify category

--- src/handlers/products/index.js
+++ src/handlers/products/index.js
@@ -1,6 +1,55 @@
 const productService = require('../../services/products');
-const HandlerMaster = require('../../templates/handlers/master');
-class ProductHandler extends HandlerMaster {
+
+class Handlers {
+    constructor(service) {
+        this.service = service;
+        this.handlers = {};
+    }
+
+    register(name, handler) {
+        this.handlers[name] = handler;
+    }
+
+    getHandler(name) {
+        return this.handlers[name];
+    }
+    getHandlers() {
+        return this.handlers;
+    }
+    registerGetAll(options={}) {
+        const GetAllHandler = require('../../templates/handlers/master/get-all');
+        const getAllHandler = new GetAllHandler(this.service, options);
+        this.register('getAll', getAllHandler.handler);
+    }
+    registerGetFromIdList(options={}) {
+        const GetFromIdListHandler = require('../../templates/handlers/master/get-from-id-list');
+        const getFromIdListHandler = new GetFromIdListHandler(this.service, options);
+        this.register('getFromIdList', getFromIdListHandler.handler);
+    }
+    registerCreateOne() {
+        const CreateOneHandler = require('./create-one');
+        const createOneHandler = new CreateOneHandler(this.service);
+        this.register('createOne', createOneHandler.handler);
+    }
+    registerGetOne() {
+        const GetOneHandler = require('../../templates/handlers/master/get-one');
+        const getOneHandler = new GetOneHandler(this.service);
+        this.register('getOne', getOneHandler.handler);
+    }
+    registerUpdateOne() {
+        const UpdateOneHandler = require('./update-one');
+        const updateOneHandler = new UpdateOneHandler(this.service);
+        this.register('updateOne', updateOneHandler.handler);
+    }
+    registerDeleteOne() {
+        const DeleteOneHandler = require('../../templates/handlers/master/delete-one');
+        const deleteOneHandler = new DeleteOneHandler(this.service);
+        this.register('deleteOne', deleteOneHandler.handler);
+    }
+    
+}
+
+class ProductHandler extends Handlers {
     constructor(service) {
         super(service);
     }
