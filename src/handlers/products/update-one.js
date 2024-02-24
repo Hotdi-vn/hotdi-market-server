2556e29f74a0220e63e9294f581e0b1c3c458099
hung.duong <hung.duong@be.com.vn>
2024-02-24 23:22:07 +0700

    update verify category

--- src/handlers/products/update-one.js
+++ src/handlers/products/update-one.js
@@ -6,7 +6,7 @@ class UpdateOneHandler {
         try {
             //verify the category is already exist
             const categoriesService = require('../../services/categories');
-            const categoryObject = categoriesService.getOne(request.body?.categoryId)
+            const categoryObject = await categoriesService.getOne(request.body?.categoryId)
             if(!categoryObject){
                 console.error({ id: request.id, code: 'CATEGORY_NOT_FOUND' });
                 reply.code(400).send({ error: { id: request.id, code: 'CATEGORY_NOT_FOUND' } })
