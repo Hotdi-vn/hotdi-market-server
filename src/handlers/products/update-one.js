f0848dda70f88d05798c9ce48793cac7db361b68
Huy <xuanhuypham1501@gmail.com>
2024-04-02 23:39:24 +0700

    feat: update inventory status based on quantity

--- src/handlers/products/update-one.js
+++ src/handlers/products/update-one.js
@@ -4,6 +4,7 @@ class UpdateOneHandler {
     }
     handler = async(request, reply) => {
         try {
+
             //verify the category is already exist
             const categoriesService = require('../../services/categories');
             const categoryObject = await categoriesService.getOne(request.body?.categoryId)
@@ -11,6 +12,12 @@ class UpdateOneHandler {
                 console.error({ id: request.id, code: 'CATEGORY_NOT_FOUND' });
                 reply.code(400).send({ error: { id: request.id, code: 'CATEGORY_NOT_FOUND' } })
             }
+
+            //check auto update Inventory Status in case InventoryManagement is true
+            if(request.body.inventoryManagementOption){
+                request.body.inventoryStatus = request.body.stockQuantity === 0 ? 'OutOfStock' : 'InStock'
+            }
+
             const userId = request.user.id;
             const _id = request.params._id;
             const data = await this.service.updateOne(_id, request.body, userId);
