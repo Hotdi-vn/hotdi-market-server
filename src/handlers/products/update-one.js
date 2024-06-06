bdf7902e6f499bf8699a4a6a282064eca273a665
Tran Minh Tien <tien.tranminh.84@gmail.com>
2024-06-06 22:32:09 +0700

    fixed lỗi bỏ qua categoryId khi không update

--- src/handlers/products/update-one.js
+++ src/handlers/products/update-one.js
@@ -1,20 +1,22 @@
+const categoriesService = require('../../services/categories');
+
 class UpdateOneHandler {
     constructor(service) {
         this.service = service;
     }
-    handler = async(request, reply) => {
+    handler = async (request, reply) => {
         try {
 
             //verify the category is already exist
-            const categoriesService = require('../../services/categories');
-            const categoryObject = await categoriesService.getOne(request.body?.categoryId)
-            if(!categoryObject){
-                console.error({ id: request.id, code: 'CATEGORY_NOT_FOUND' });
-                reply.code(400).send({ error: { id: request.id, code: 'CATEGORY_NOT_FOUND' } })
+            if (request.body.categoryId) {
+                const categoryObject = await categoriesService.getOne(request.body?.categoryId)
+                if (!categoryObject) {
+                    console.error({ id: request.id, code: 'CATEGORY_NOT_FOUND' });
+                    reply.code(400).send({ error: { id: request.id, code: 'CATEGORY_NOT_FOUND' } })
+                }
             }
-
             //check auto update Inventory Status in case InventoryManagement is true
-            if(request.body.inventoryManagementOption){
+            if (request.body.inventoryManagementOption) {
                 request.body.inventoryStatus = request.body.stockQuantity === 0 ? 'OutOfStock' : 'InStock'
             }
 
