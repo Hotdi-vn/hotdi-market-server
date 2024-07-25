f6f891865bbd060f699f5ccefaed5cc89771f431
tranminhtien <tien.tranminh.84@gmail.com>
2024-07-26 06:49:00 +0700

    Update shop.js

--- src/models/shop.js
+++ src/models/shop.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const shopSettings = require('../settings/shop');
-
+const fileModel = require('./file');
 const schema = new mongoose.Schema(shopSettings.getMongooseSchema());
 const model = mongoose.model('shop', schema);
 
